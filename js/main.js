26f9317b9aef6a16939c8e9a7d02c8673e62343a
Michael Edelstone <michael.edelstone@gmail.com>
2016-11-18 15:58:14 -0600

    added footer, some image edits, minor style changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-// No 300ms mobile delay on mobile
+// Remove 300ms tapping delay on mobile
 window.addEventListener('load', function() {
 	FastClick.attach(document.body);
 }, false);
+
+//Get and display page load time
+//calculate the time before calling the function in window.onload
+var beforeload = (new Date()).getTime();
+
+function getPageLoadTime(){
+	//calculate the current time in afterload
+	var afterload = (new Date()).getTime();
+	// now use the beforeload and afterload to calculate the seconds
+	seconds = (afterload-beforeload) / 1000;
+	// Place the seconds in the innerHTML to show the results
+	$(".load-time").text('This page loaded in ' + seconds + ' seconds.');
+}
+
+window.onload = getPageLoadTime;
