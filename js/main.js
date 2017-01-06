5cab9847859805157b8cedd640c6fef21f647a66
Michael Edelstone <michael.edelstone@gmail.com>
2017-01-06 16:17:58 -0600

    new about page, some CSS updates

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ var beforeload = (new Date()).getTime();
 function getPageLoadTime(){
 	//calculate the current time in afterload
 	var afterload = (new Date()).getTime();
-	// now use the beforeload and afterload to calculate the seconds
-	seconds = (afterload-beforeload) / 1000;
+	// now use the beforeload and afterload to calculate the seconds and round to 2 decimals max
+	seconds = ((afterload-beforeload) / 1000).toFixed(2);
 	// Place the seconds in the innerHTML to show the results
-	$(".load-time").text('This page loaded in ' + seconds + ' seconds.');
+	$(".load-time").text('This page loaded in ' + seconds + ' seconds');
 }
 
 window.onload = getPageLoadTime;
